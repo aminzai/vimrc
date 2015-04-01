ad3357292b6505a949ee1760464df6925cff71fa
Amin Wang <lagunawang@gmail.com>
2015-04-01 09:03:56 +0800

    add update YouCompleteMe

--- update.sh
+++ update.sh
@@ -2,7 +2,15 @@
 git pull origin master
 git submodule init
 git submodule update
+git submodule update --init --recursive
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 #git submodule foreach git pull
 git submodule foreach git pull origin master
+
+
+#Compile YouCompleteMe
+CUR=${PWD}
+cd bundle/YouCompleteMe/
+./install.sh
+cd ${CUR}
